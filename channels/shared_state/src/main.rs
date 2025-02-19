cf36eda2b985b81cec0c804016b7d31b8dcd6a53
Elyan <ekenimohelyanpheta@gmail.com>
2025-02-19 07:23:38 -0800

    Added Rust_100 as a normal folder

--- channels/shared_state/src/main.rs
+++ channels/shared_state/src/main.rs
@@ -1,6 +1,20 @@
 // SHARED STATE CONCURENCY
+use std::sync::Mutex;
 
 fn main() {
-    println!("Hello, world!");
+    let m = Mutex::new(5);
+
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
+    }
+    println!("m, {m:?}!");
     // do not communicate by sharing memory; instead, share memory by communicating
 }
+
+
+// MUTEX = mutual exclusive
+// Mutexes have a reputation for being difficult to use because you have to remember two rules:
+
+// You must attempt to acquire the lock before using the data.
+// When you’re done with the data that the mutex guards, you must unlock the data so other threads can acquire the lock.
