878cbfea23c173aa55a20011fa338f858c00dd03
Elyan <ekenimohelyanpheta@gmail.com>
2025-03-27 09:00:32 -0700

    tokio prject

---
+++ tokio/src/main.rs
@@ -0,0 +1,14 @@
+use mini_redis::{client, Result};
+#[tokio::main]
+
+async fn main() -> Result<()> {
+    // open a redis connection
+    let mut client = client::connect("127.0.0.1:6379").await?;
+
+    // set the key "hello" with value "world"
+    client.set("hello", "world".into()).await?;
+    let result = client.get("hello").await?;
+    println!("got the value from the server; result={:?}", result);
+
+    Ok(())
+}
