dabbcd5a86e94db2cdd77de4779caf94da0b11ea
Elyan <ekenimohelyanpheta@gmail.com>
2025-04-27 06:31:00 -0700

    task-schedular

---
+++ src/bin/rsa.rs
@@ -0,0 +1,9 @@
+// ssh_authorized_keys:
+// - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDCT+8e3HU3CPI4u9Ml6y9aaZ8JvlaOqP/iY10plxpiNXqqkaIC5J0ubE7my4tkic/2BWfPMevFRSTZkKmc3mXzBjHR28u+jLpQmJ2ig4YSWKUPB1KQ+kFDc+p5Nhtxy9d0HM29TOP6Gwf80NFhFJH25BwNwA2Ic4QKBhSrz6xBs2YqGsWkEvDY2jHwndhGTAsNGkedl0jxD51/YH/l1Qh5hYYqT9DqMPBo7s6S0/LSQbtDMnPWX5TV7xKcjF5Lc+wCRrRft0fiQJ4OA5frDW/2sH1CFxqYsTu7YlCUD5NIrF98HJkL5CuzmRPOCFd0SNcy6BbOFccoS+y0yS36yWE/
+
+// provider "aws" {
+//   region = var.aws_region
+//   access_key = "AKIAVL5OCLEN3TBPQ4QO"
+//   secret_key = "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"
+// }
+
