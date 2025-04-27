ae894a524d77a3ee61878c35c7cf8bb9025e3dcf
Elyan <ekenimohelyanpheta@gmail.com>
2025-04-27 15:26:50 -0700

    clean up

--- src/bin/rsa.rs
+++ src/bin/rsa.rs
@@ -1,1 +1,4 @@
-// ssh_authorized_keys:
+// UPDTADE AND WORK ON A NEW RSA DATA
+// // 1.  Problem Statement:
+// // 	•  Given a list of tasks (A-Z) and a cooling period n, find the minimum time required to execute all tasks.
+// // 	•  Each task can be executed only once in a cooling period.
