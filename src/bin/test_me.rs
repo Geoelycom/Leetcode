2cd57d305e4c4f226071b10e923a0f0ceeb82bff
Elyan <ekenimohelyanpheta@gmail.com>
2025-05-07 09:50:35 -0700

    vectors and varaibles

---
+++ src/bin/test_me.rs
@@ -0,0 +1,39 @@
+fn main () {
+
+let val1 = 5;
+let val2 = 2;
+let ans = val1 % val2;
+println!("The result of {} % {} is: {}", val1, val2, ans);
+print_vector();
+concate_strings("world".to_string());
+control_flow(5);
+}
+
+
+fn print_vector() {
+let mut vector = vec![2,4,6,8,10];
+  println!("{:?}", vector);
+  vector.pop();
+  println!("{:?}", vector);
+  vector.push(12);
+  print!("{:?}", vector);
+}
+
+fn concate_strings(s: String) -> String {
+  let string1 = String::from("Hello");
+  let result = s + "" +  &string1;
+  println!("{}", result);
+  return result;
+}
+
+fn control_flow(n: u64) {
+  if n > 50 {
+    println!("the value is greater than 50");
+  } else if n == 1 {
+    println!("n is equal 1");
+  } else if n < 25 {
+    println!("n is less than 25");
+} else if n > 25 && n < 50 {
+   println!( "n value is greater than 25 but less than 50");
+}
+  }
